5dcdafff63c349074c3c5ffaf142222aa18dfb37
Luu Thai Vuong <vuongluu@MacBook-Pro-cua-Luu.local>
2021-06-09 12:07:56 +0700

    update new code

--- src/core/cms/src/Views/auth/components/form/select.blade.php
+++ src/core/cms/src/Views/auth/components/form/select.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
     <label for="exampleInputEmail1">{{ $label }}</label>
-    <select class="form-control select2" name="category_id">
+    <select class="form-control select2" name="{{ $name }}">
         <option value="">---</option>
         @foreach($options as $option)
         <option value="{{ $option->id }}" {{ $item->$name === $option->id ? 'selected' : '' }}>{{ $option->name }}</option>
