36b901a78eb6d51bc091e1319eb3f3bb0210645c
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2021-07-25 17:41:44 +0700

    add web source

--- src/core/web/src/Views/components/menu_mobile.blade.php
+++ src/core/web/src/Views/components/menu_mobile.blade.php
@@ -1,23 +1,26 @@
 <!-- Menu mobile -->
 <div id="mySidenav" class="sidenav menu_mobile hidden-md hidden-lg">
     <div class="top_menu_mobile">
-        <span class="close_menu"><img src="image/catalog/logo/logo2.png" alt="Spatina Sports Style Store"></span>
+        <span class="close_menu"><img src="{{ $config->getWebLogo() }}" alt="{{ $config->web_title }}"></span>
     </div>
     <div class="content_memu_mb">
         <div class="link_list_mobile">
             <ul class="ct-mobile hidden"></ul>
             <ul class="ct-mobile">
                 <li class="level0 level-top parent level_ico">
-                    <a href="index.html">Trang chủ</a>
+                    <a href="/">{{ trans('web::label.home') }}</a>
                 </li>
                 <li class="level0 level-top parent level_ico">
-                    <a href="san-pham.html">Sản phẩm</a>
+                    <a href="{{ route('page.about') }}">{{ trans('web::label.about') }}</a>
                 </li>
                 <li class="level0 level-top parent level_ico">
-                    <a href="tin-tuc.html">Tin tức</a>
+                    <a href="{{ route('product.index') }}">{{ trans('web::label.product') }}</a>
                 </li>
                 <li class="level0 level-top parent level_ico">
-                    <a href="lien-he.html">Liên hệ</a>
+                    <a href="{{ route('post.index') }}">{{ trans('web::label.news') }}</a>
+                </li>
+                <li class="level0 level-top parent level_ico">
+                    <a href="{{ route('contact.index') }}">{{ trans('web::label.contact') }}</a>
                 </li>
             </ul>
         </div>
