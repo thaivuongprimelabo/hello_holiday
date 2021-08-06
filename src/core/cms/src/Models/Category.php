86ac8dd72ea6e928982145feb6ef5d2ed477d596
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2021-08-06 21:25:15 +0700

    fix bug

--- src/core/cms/src/Models/Category.php
+++ src/core/cms/src/Models/Category.php
@@ -14,7 +14,7 @@ class Category extends Model
 
     public function products()
     {
-        return $this->hasMany(Product::class, 'category_parent_id', 'id');
+        return $this->hasMany(Product::class, 'category_id', 'id');
     }
 
     public function childCategories()
