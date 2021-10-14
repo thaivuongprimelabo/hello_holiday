2fa3ce0b85d97258d85aacbe1905a166d2a6edd9
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2021-10-14 19:54:41 +0700

    aaaa

--- core/cms/src/Views/auth/layouts/form.blade.php
+++ core/cms/src/Views/auth/layouts/form.blade.php
@@ -1,4 +1,4 @@
 @extends('cms::auth.layouts.main')
 @section('scripts')
-<script type="text/javascript" src="{{ asset('cms/form.js') }}"></script>
+<script type="text/javascript" src="{{ asset('cms/form.js?t=' . time()) }}"></script>
 @endsection
