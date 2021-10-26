fd26998cdfb7be74d44e532e7a8cf515d29300df
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2021-10-26 21:59:31 +0700

    Add license

--- public/cms/plugins/ckfinder/config.php
+++ public/cms/plugins/ckfinder/config.php
@@ -32,8 +32,8 @@ $config['authentication'] = function () {
 /*============================ License Key ============================================*/
 // https://ckeditor.com/docs/ckfinder/ckfinder3-php/configuration.html#configuration_options_licenseKey
 
-$config['licenseName'] = '';
-$config['licenseKey']  = '';
+$config['licenseName'] = 'motor2k.com';
+$config['licenseKey']  = '29T9K4298YH899BSYJRGJFWEYWQMD';
 
 /*============================ CKFinder Internal Directory ============================*/
 // https://ckeditor.com/docs/ckfinder/ckfinder3-php/configuration.html#configuration_options_privateDir
