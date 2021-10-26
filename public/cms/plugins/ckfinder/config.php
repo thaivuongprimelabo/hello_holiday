dcc9b1fd468003282310906846c5f237b4ef615c
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2021-10-26 11:28:03 +0700

    Add license

--- public/cms/plugins/ckfinder/config.php
+++ public/cms/plugins/ckfinder/config.php
@@ -32,8 +32,8 @@ $config['authentication'] = function () {
 /*============================ License Key ============================================*/
 // https://ckeditor.com/docs/ckfinder/ckfinder3-php/configuration.html#configuration_options_licenseKey
 
-$config['licenseName'] = '';
-$config['licenseKey']  = '';
+$config['licenseName'] = 'localhost';
+$config['licenseKey']  = '1GZGMGBWP7PH6QP8PVG64Z656USJ3';
 
 /*============================ CKFinder Internal Directory ============================*/
 // https://ckeditor.com/docs/ckfinder/ckfinder3-php/configuration.html#configuration_options_privateDir
