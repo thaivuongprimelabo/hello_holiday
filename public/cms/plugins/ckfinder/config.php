8340bc79b04098476c4a9a0c1d23a441e48d2797
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2021-10-27 15:05:26 +0700

    Fix config

--- public/cms/plugins/ckfinder/config.php
+++ public/cms/plugins/ckfinder/config.php
@@ -31,10 +31,12 @@ $config['authentication'] = function () {
 
 /*============================ License Key ============================================*/
 // https://ckeditor.com/docs/ckfinder/ckfinder3-php/configuration.html#configuration_options_licenseKey
-if (strpos($_SERVER['HTTP_HOST'], 'motor2k.com') !== false) {
+$hostname = str_replace('https://wwww.', '', $_SERVER['SERVER_NAME']);
+$hostname = str_replace('wwww.', '', $_SERVER['SERVER_NAME']);
+if ($hostname == 'motor2k.com') {
     $config['licenseName'] = 'motor2k.com';
     $config['licenseKey']  = 'Y2D28QWGADSMEBUQUM6UJQW7VL1C3';
-} if (strpos($_SERVER['HTTP_HOST'], 'thaivuong.com') !== false) {
+} if ($hostname == 'thaivuong.com') {
     $config['licenseName'] = 'thaivuong.com';
     $config['licenseKey']  = 'XYUY7AYJ8284U9TGVF77EKXENVPCE';
 } else {
